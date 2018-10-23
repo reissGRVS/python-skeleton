781c76b17105303a2caa16fdc9541f2ecc53ef10
reissgrvs <rwag365@gmail.com>
2018-10-23 12:36:39 +0100

    Taking absolute of values as np deals with signed ints weirdly

--- q1.py
+++ q1.py
@@ -10,18 +10,16 @@ def question01(portfolios):
   #Loop from MSB only to all bits
   for i in reversed(range(16)):
       #Get relevant bits for all portfolios
-      prefixes = set([portfolio >> i for portfolio in portfolios])
+      prefixes = set([abs(portfolio >> i) for portfolio in portfolios])
       
       #Shift our previous answer up 1 bit so that it muches new bit selections above
       answer <<= 1
       #We want to increase our previous max, as we are going 1 bit at a time, this is the increased value
       candidate = answer + 1
-
       #For each prefix check if there is a another prefix that XORs to make our candidate max value
       for p in prefixes:
-          if candidate ^ p in prefixes:
+          if abs(candidate ^ p) in prefixes:
               #If a match is found update the answer
               answer = candidate
               break
-
   return answer
