47190c3d1088fa3415f1cf5d1709a167a9fb1334
reissgrvs <rwag365@gmail.com>
2018-10-23 11:56:42 +0100

    Updated comments on q1

--- q1.py
+++ q1.py
@@ -6,14 +6,22 @@ import numpy as np
 def question01(portfolios):
   # modify and then return the variable below
   answer = 0
+  
+  #Loop from MSB only to all bits
   for i in reversed(range(16)):
+      #Get relevant bits for all portfolios
       prefixes = set([portfolio >> i for portfolio in portfolios])
+      
+      #Shift our previous answer up 1 bit so that it muches new bit selections above
       answer <<= 1
+      #We want to increase our previous max, as we are going 1 bit at a time, this is the increased value
       candidate = answer + 1
 
-      #For each prefix check if there is an optimal XOR for current candidate answer
+      #For each prefix check if there is a another prefix that XORs to make our candidate max value
       for p in prefixes:
           if candidate ^ p in prefixes:
+              #If a match is found update the answer
               answer = candidate
               break
+
   return answer
